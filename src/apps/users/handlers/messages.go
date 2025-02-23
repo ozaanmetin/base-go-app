6820f962bf5d2e1282438621368a9e6202ca8f36
Ozan Metin <ozanmetin99@gmail.com>
2025-02-23 17:46:55 +0300

    users handler messages constants added.

---
+++ src/apps/users/handlers/messages.go
@@ -0,0 +1,9 @@
+package messages
+
+const (
+	RequestValidationErrorMessage = "Request Validation Error"
+	InvalidCredentialsMessage     = "Invalid Credentials"
+	LoginSuccessfulMessage        = "Login Successful"
+	TokenGenerationErrorMessage   = "Token Generation Error"
+	TokenRefreshedMessage         = "Token Refreshed"
+)
