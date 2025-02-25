fe441d0a6e87f563b136b2e2b3b9624b91adeadc
Ozan Metin <ozanmetin99@gmail.com>
2025-02-25 11:40:05 +0300

    migrations for role added and rollback added.

---
+++ src/database/migrations/users/20250225082950_add_role_to_users.down.sql
@@ -0,0 +1,3 @@
+-- Add role column to users table
+ALTER TABLE users
+DROP COLUMN role;
---
+++ src/database/migrations/users/20250225082950_add_role_to_users.up.sql
@@ -0,0 +1,3 @@
+-- Add role column to users table
+ALTER TABLE users
+ADD COLUMN role VARCHAR(50) NOT NULL DEFAULT 'member';
