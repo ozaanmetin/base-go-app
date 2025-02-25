3935cae647d36103dcd79f121989e21fc80e1c99
Ozan Metin <ozanmetin99@gmail.com>
2025-02-25 23:52:46 +0300

    mock testing ping-pong api removed. and routes are removed from this file, setupRoutes private function added.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,52 +1,34 @@
 package main
 
 import (
-	"base-go-app/src/apps/users/models"
 	"base-go-app/src/common/utils/environment"
 	"base-go-app/src/database"
+	usersRouters "base-go-app/src/routers/users"
 
 	"github.com/gin-gonic/gin"
 
-	jwtHandlers "base-go-app/src/apps/users/handlers/jwt"
-	authenticationMiddlewares "base-go-app/src/middlewares/authentication"
 	securityMiddlewares "base-go-app/src/middlewares/security"
 )
 
 func main() {
+	// Initialize Environment Variables
 	environment.InitalizeEnv()
+	// Connect to Postgres Database
 	database.ConnectPostgres()
+	// Setup Router and Run
+	engine := setupRouter()
+	engine.Run()
+}
 
+func setupRouter() *gin.Engine {
 	r := gin.Default()
+	// Security Middlewares
 	r.Use(securityMiddlewares.StrictHostValidationMiddleware())
 	r.Use(securityMiddlewares.CorsMiddleware())
-
 	// Api Group
 	api := r.Group("/api")
 	// Authentication
-	{
-		auth := api.Group("/auth")
-		{
-			auth.POST("/login", jwtHandlers.LoginHandler)
-			auth.POST("/refresh", jwtHandlers.RefreshHandler)
-		}
-	}
-	usersGroup := api.Group("/users")
-	{
-		{
-			usersGroup.GET("/", func(c *gin.Context) {
-				var users []models.User
-				database.PostgresContext.Find(&users)
-				c.JSON(200, gin.H{"data": users})
-			})
-		}
-	}
-
-	// Authenticated Group
-	authenticated := api.Group("/authenticated")
-	authenticated.Use(authenticationMiddlewares.AuthMiddleware(), authenticationMiddlewares.CheckRoleMiddleware("admin"))
-	authenticated.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
-
-	r.Run()
+	usersRouters.AuthenticationRouter(api)
+	usersRouters.UserCrudRouter(api)
+	return r
 }
