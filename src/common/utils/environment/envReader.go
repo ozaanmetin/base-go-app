60bdeffe529efc986bd27b8a0e560bccd696cccc
Ozan Metin <ozanmetin99@gmail.com>
2025-02-25 10:57:09 +0300

    GetAsSlice and GetAsBool functions are added in order to read from environment

--- src/common/utils/environment/envReader.go
+++ src/common/utils/environment/envReader.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -18,6 +19,7 @@ func InitalizeEnv() {
 }
 
 func GetAsInt(key string, defaultValue int) int {
+	// returns as int
 	value := os.Getenv(key)
 	if value == "" {
 		return defaultValue
@@ -28,5 +30,28 @@ func GetAsInt(key string, defaultValue int) int {
 		return defaultValue
 	}
 
+	return parsedValue
+}
+
+func GetAsSlice(key string, defaultValue []string) []string {
+	// returns as slice with comma separated values
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return strings.Split(value, ",")
+}
+
+func GetAsBool(key string, defaultValue bool) bool {
+	// returns as bool
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	parsedValue, err := strconv.ParseBool(value)
+	if err != nil {
+		return defaultValue
+	}
 	return parsedValue
 }
