abe5a26da230707074926067bb34c2c3759615d9
Ozan Metin <ozanmetin99@gmail.com>
2025-02-25 23:53:24 +0300

    user repository updated with basic crud capabilities.

--- src/apps/users/repositories/userRepository.go
+++ src/apps/users/repositories/userRepository.go
@@ -15,12 +15,20 @@ func CreateUserRepository(db *gorm.DB) *UserRepository {
 	return &UserRepository{DB: db}
 }
 
-func (repo *UserRepository) FindAll() {
-	// Implementation here
+func (repo *UserRepository) FindAll() ([]models.User, error) {
+	var users []models.User
+	if err := repo.DB.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 
-func (repo *UserRepository) FindByID() {
-	// Implementation here
+func (repo *UserRepository) FindByID(id string) (*models.User, error) {
+	var user models.User
+	if err := repo.DB.First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
 func (repo *UserRepository) FindByUsername(username string) (*models.User, error) {
@@ -34,4 +42,16 @@ func (repo *UserRepository) FindByUsername(username string) (*models.User, error
 
 	}
 	return &user, nil
+}
+
+func (repo *UserRepository) Create(user *models.User) error {
+	return repo.DB.Create(user).Error
+}
+
+func (repo *UserRepository) Update(user *models.User) error {
+	return repo.DB.Save(user).Error
+}
+
+func (repo *UserRepository) Delete(id string) error {
+	return repo.DB.Delete(&models.User{}, id).Error
 }
