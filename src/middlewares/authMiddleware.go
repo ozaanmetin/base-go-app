8e11c0e5c9a74056ec5f36bbe6354af9bb2d4e01
Ozan Metin <ozanmetin99@gmail.com>
2025-02-25 10:57:27 +0300

    AuthMiddleware added for authencation on api's

---
+++ src/middlewares/authMiddleware.go
@@ -0,0 +1,51 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	jwtSigningKey := []byte(os.Getenv("JWT_SIGNING_KEY"))
+	return func(c *gin.Context) {
+		// Get the token from the Authorization header
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(
+				http.StatusUnauthorized,
+				gin.H{"error": "Authorization header is required"},
+			)
+			return
+		}
+		// Trim the Bearer prefix and validate the token
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(jwtSigningKey), nil
+		})
+
+		if err != nil {
+			c.AbortWithStatusJSON(
+				http.StatusUnauthorized,
+				gin.H{"error": "Invalid Token"},
+			)
+		}
+		// Set the granted userId in the context
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("userId", claims["sub"])
+			c.Next()
+		} else {
+			c.AbortWithStatusJSON(
+				http.StatusUnauthorized,
+				gin.H{"error": "Invalid Token"},
+			)
+		}
+	}
+}
