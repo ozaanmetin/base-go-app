1e01fe704884712224dd2b25aa8264e937b48618
Ozan Metin <ozanmetin99@gmail.com>
2025-02-28 19:50:12 +0300

    envUtils created.

---
+++ config/settings/environment/envUtils.go
@@ -0,0 +1,56 @@
+package environment
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+func InitalizeDotEnv() {
+	// Load the .env file if it exists
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func GetAsInt(key string, defaultValue int) int {
+	// returns as int
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	parsedValue, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+
+	return parsedValue
+}
+
+func GetAsSlice(key string, defaultValue []string) []string {
+	// returns as slice with comma separated values
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return strings.Split(value, ",")
+}
+
+func GetAsBool(key string, defaultValue bool) bool {
+	// returns as bool
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	parsedValue, err := strconv.ParseBool(value)
+	if err != nil {
+		return defaultValue
+	}
+	return parsedValue
+}
