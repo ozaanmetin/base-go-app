a6eca1d92338448cb81b8c5d8c819a7026d17dd6
Ozan Metin <ozanmetin99@gmail.com>
2025-02-28 19:50:14 +0300

    Create envValues.go

---
+++ config/settings/environment/envValues.go
@@ -0,0 +1,23 @@
+package environment
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+)
+
+func GetBaseDir() (string, error) {
+	// Get the current file's directory, not the working directory
+	_, currentFilePath, _, ok := runtime.Caller(0)
+	if !ok {
+		return "", fmt.Errorf("unable to get current file path")
+	}
+
+	// Get the directory of the current file
+	baseDir := filepath.Dir(currentFilePath)
+	baseDir, err := filepath.Abs(filepath.Join(baseDir, "..", ".."))
+	if err != nil {
+		return "", err
+	}
+	return baseDir, nil
+}
